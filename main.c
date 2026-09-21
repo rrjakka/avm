4269feb88e98d6ea3322b1689f37493a108764a1
rrjakka <timyr2005kadirov@gmail.com>
2026-09-21 17:25:59 +0500

    added memory

--- main.c
+++ main.c
@@ -2,13 +2,24 @@
 #include <stdint.h>
 #include <stdio.h>
 
+static constexpr uint64_t AVM_MEMORY_SIZE = 1024;
+static constexpr uint8_t AVM_REGISTER_SIZE = 16;
+
 typedef enum
 {
     INSTRUCTION_TYPE_NOP=0,
-    INSTRUCTION_TYPE_PUSH,
-    INSTRUCTION_TYPE_ADD,
-    INSTRUCTION_TYPE_PRINT,
     INSTRUCTION_TYPE_HALT,
+
+    INSTRUCTION_TYPE_MOVE,
+
+    INSTRUCTION_TYPE_LOAD_IMMEDIATE,
+    INSTRUCTION_TYPE_LOAD,
+
+    INSTRUCTION_TYPE_STORE,
+
+    INSTRUCTION_TYPE_ADD,
+
+    INSTRUCTION_TYPE_PRINT,
 } instruction_type_t;
 
 typedef struct
@@ -21,10 +32,12 @@ typedef struct
 
 typedef struct
 {
-    int64_t registers[16];
+    int64_t registers[AVM_REGISTER_SIZE];
     instruction_t* program;
     uint64_t pip; // program instruction pointer
     bool halt;
+
+    int64_t memory[AVM_MEMORY_SIZE];
 } avm_t; // arctic virtual machine
 
 static void avm_run(avm_t* avm)
@@ -33,23 +46,39 @@ static void avm_run(avm_t* avm)
     {
         const instruction_t* instruction = &avm->program[avm->pip];
 
+        const int64_t operand_0 = instruction->operand_0;
+        const int64_t operand_1 = instruction->operand_1;
+        const int64_t operand_2 = instruction->operand_2;
+
         switch (instruction->type)
         {
             case INSTRUCTION_TYPE_NOP:
                 avm->pip += 1;
                 break;
-            case INSTRUCTION_TYPE_PUSH:
-                avm->registers[instruction->operand_0] = instruction->operand_1;
+            case INSTRUCTION_TYPE_LOAD_IMMEDIATE:
+                avm->registers[operand_0] = operand_1;
+                avm->pip += 1;
+                break;
+            case INSTRUCTION_TYPE_LOAD:
+                avm->registers[operand_0] = avm->memory[avm->registers[operand_1]];
+                avm->pip += 1;
+                break;
+            case INSTRUCTION_TYPE_STORE:
+                avm->memory[avm->registers[operand_0]] = avm->registers[operand_1];
+                avm->pip += 1;
+                break;
+            case INSTRUCTION_TYPE_MOVE:
+                avm->registers[operand_0] = avm->registers[operand_1];
                 avm->pip += 1;
                 break;
             case INSTRUCTION_TYPE_ADD:
-                const int64_t left = avm->registers[instruction->operand_1];
-                const int64_t right = avm->registers[instruction->operand_2];
-                avm->registers[instruction->operand_0] = left + right;
+                const int64_t left = avm->registers[operand_1];
+                const int64_t right = avm->registers[operand_2];
+                avm->registers[operand_0] = left + right;
                 avm->pip += 1;
                 break;
             case INSTRUCTION_TYPE_PRINT:
-                printf("%" PRIi64 "\n", avm->registers[instruction->operand_0]);
+                printf("%" PRIi64 "\n", avm->registers[operand_0]);
                 avm->pip += 1;
                 break;
             case INSTRUCTION_TYPE_HALT:
@@ -59,10 +88,37 @@ static void avm_run(avm_t* avm)
     }
 }
 
-static instruction_t instructions_push(const int64_t target, const int64_t value)
+static instruction_t instructions_load_immediate(const int64_t target, const int64_t value)
 {
     return (instruction_t) {
-        .type = INSTRUCTION_TYPE_PUSH,
+        .type = INSTRUCTION_TYPE_LOAD_IMMEDIATE,
+        .operand_0 = target,
+        .operand_1 = value,
+    };
+}
+
+static instruction_t instructions_load(const int64_t target, const int64_t reg)
+{
+    return (instruction_t) {
+        .type = INSTRUCTION_TYPE_LOAD,
+        .operand_0 = target,
+        .operand_1 = reg,
+    };
+}
+
+static instruction_t instructions_store(const int64_t target, const int64_t value)
+{
+    return (instruction_t) {
+        .type = INSTRUCTION_TYPE_LOAD,
+        .operand_0 = target,
+        .operand_1 = value,
+    };
+}
+
+static instruction_t instructions_move(const int64_t target, const int64_t value)
+{
+    return (instruction_t) {
+        .type = INSTRUCTION_TYPE_MOVE,
         .operand_0 = target,
         .operand_1 = value,
     };
@@ -93,20 +149,27 @@ static instruction_t instructions_halt()
     };
 }
 
-// stack -> register
 int main()
 {
+    avm_t avm = {0};
+
+    constexpr uint64_t value_address = 69;
+
+    avm.memory[value_address] = 10;
+    avm.memory[value_address + 1] = 5;
+
+    avm.halt = false;
+
     instruction_t program[] = {
-        instructions_push(0, 10),
-        instructions_push(1, 5),
-        instructions_add(2, 0, 1),
-        instructions_print(2),
+        instructions_load_immediate(0, value_address),
+        instructions_load(1, 0),
+        instructions_load_immediate(0, value_address + 1),
+        instructions_load(2, 0),
+        instructions_add(0, 1, 2),
+        instructions_print(0),
         instructions_halt(),
     };
 
-    avm_t avm = {0};
-
-    avm.halt = false;
     avm.program = program;
 
     avm_run(&avm);
